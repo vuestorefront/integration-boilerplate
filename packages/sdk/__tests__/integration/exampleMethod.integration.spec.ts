b0bc2e7b0004ac52188b48a757293558ea1f2e69
Wojciech Sikora <35867383+WojtekTheWebDev@users.noreply.github.com>
2023-03-24 15:42:49 +0100

    chore: apply sdk standards (#7)

--- packages/sdk/__tests__/integration/exampleMethod.integration.spec.ts
+++ packages/sdk/__tests__/integration/exampleMethod.integration.spec.ts
@@ -1,10 +1,10 @@
-import { sdk } from '../../sdk.config';
+import { sdk } from './__config__/sdk.config';
 
 describe('[Integration Boilerplate SDK][integration] exampleMethod', () => {
   it('makes a request to the middleware', async () => {
     const EXPECTED_RESPONSE = { success: true };
 
-    const res = await sdk.commerce.exampleMethod({
+    const res = await sdk.boilerplate.exampleMethod({
       id: 1,
     });
 
