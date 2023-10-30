e4c207a8a48de0775e9fe31fe088c729348b9836
rohrig <richard.t.rohrig@gmail.com>
2023-10-30 16:23:41 +0100

    feat: update nuxt page template

--- cli/dist/chunks/add.mjs
+++ cli/dist/chunks/add.mjs
@@ -109,29 +109,26 @@ const nuxtPageMethod = ({ name }) => ({
   path: `playground/app/pages/methods/${name}.vue`,
   contents: `
   <template>
-  <div class="flex justify-center items-center h-screen">
-    <div class="p-5 w-96">
-      <p class="typography-text-xs md:typography-text-sm font-bold tracking-widest text-neutral-500 uppercase">
-        Let's go
-      </p>
-      <h1 class="typography-headline-2 md:typography-headline-1 md:leading-[67.5px] font-bold mt-2 mb-4">
-        Build something amazing
-      </h1>
-      <p>
-       ${name}()
-      </p>
-      <div class="box">
-        <!-- <JsonViewer :value="jsonData" copyable boxed sort theme="light"  @onKeyClick="keyClick"/> -->
-        <h4>Response</h4>
-        <JsonViewer class="min-h-[800px] min-w-[500px]" :value="res" expandDepth="5" expanded copyable boxed sort
-          theme="dark" />
-      </div>
-      <div class="flex flex-col md:flex-row gap-4 mt-6">
-        <SfButton @click="callEndpoint" size="lg"> call </SfButton>
-        <SfButton @click="reset" size="lg" variant="secondary" class="bg-white"> reset </SfButton>
-      </div>
+    <div class="flex justify-center items-center h-screen">
+        <div class="p-5 w-96">
+            <h1 class="typography-headline-2 font-bold mt-2 mb-4 text-green-500">
+                Build something amazing
+            </h1>
+            <p class="text-gray-50">
+                ${name}
+            </p>
+            <div class="box">
+                <!-- <JsonViewer :value="jsonData" copyable boxed sort theme="light"  @onKeyClick="keyClick"/> -->
+                <h4 class="text-gray-50">Response</h4>
+                <JsonViewer class="min-h-[800px] min-w-[500px]" :value="res" expandDepth="5" expanded copyable boxed sort
+                    theme="dark" />
+            </div>
+            <div class="flex flex-col md:flex-row gap-4 mt-6">
+                <SfButton @click="callEndpoint" size="lg"> call </SfButton>
+                <SfButton @click="reset" size="lg" variant="secondary" class="bg-white"> reset </SfButton>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script lang="ts" setup>
@@ -140,15 +137,15 @@ import { sdk } from '~/sdk.config';
 import { JsonViewer } from "vue3-json-viewer"
 import "vue3-json-viewer/dist/index.css";
 
-const res = useState('waiting to call ${name}() ...');
+const res = useState('waiting to call ${name} ...');
 
 async function callEndpoint() {
-  const { data } = await sdk.boilerplate.${name}('test');
-  res.value = data
+    const { data } = await sdk.boilerplate.exampleMethod('test');
+    res.value = data
 }
 
 function reset() {
-  res.value = 'waiting to call ${name}() ...'
+    res.value = 'waiting to call ${name} ...'
 }
 <\/script>
 `
